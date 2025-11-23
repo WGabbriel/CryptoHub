424975e0017a12e6ed0628840329a9fa1e16b04b
Wallace Gabriel <gs008963@gmail.com>
2025-11-22 22:38:25 -0300

    add TopAppBar in MainActivity with search and profile icons

--- app/src/main/java/com/wgabbriel/cryptohub/MainActivity.kt
+++ app/src/main/java/com/wgabbriel/cryptohub/MainActivity.kt
@@ -5,9 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.wgabbriel.cryptohub.ui.nav.BottomNavBar
@@ -16,6 +23,7 @@ import com.wgabbriel.cryptohub.ui.nav.MainNavHost
 import com.wgabbriel.cryptohub.ui.theme.CryptoHubTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,7 +31,33 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             CryptoHubTheme {
                 Scaffold(
-                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("CryptoHub")
+                            },
+                            actions = {
+                                IconButton(onClick = {
+                                    // todo Pesquisa
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Default.Search,
+                                        contentDescription = "Search"
+                                    )
+                                }
+                                IconButton(onClick = {
+                                    //todo menu lateral
+                                }) {
+                                    Icon(
+                                        imageVector =
+                                            Icons.Default.Person,
+                                        contentDescription = "Profile"
+                                    )
+                                }
+                            }
+                        )
+                    },
+
                     bottomBar = {
                         val items = listOf(
                             BottomNavItem.MarketButton,
